7196104cc8e65d99cb715899e7f8ca0648e549a0
aollivierre <abdullahollivierre@gmail.com>
2024-08-27 08:28:01 -0700

    removing Sub ID

--- MigratefromAutomationAccount/1-call.ps1
+++ MigratefromAutomationAccount/1-call.ps1
@@ -1,4 +1,4 @@
-# & "C:\Code\CB\Azure\Antea\1-Migrate-From-UpdateManager.ps1" -AutomationAccountResourceId "/subscriptions/28c8da4f-f7e4-40b4-9fe8-fd53e4581d26/resourceGroups/defaultresourcegroup-cus/providers/Microsoft.Automation/automationAccounts/Automate-28c8da4f-f7e4-40b4-9fe8-fd53e4581d26-CUS
+# & "C:\Code\CB\Azure\Antea\1-Migrate-From-UpdateManager.ps1" -AutomationAccountResourceId "/subscriptions/[AzSubID]/resourceGroups/defaultresourcegroup-cus/providers/Microsoft.Automation/automationAccounts/Automate-[AzSubID]-CUS
 # AzureCloud" -$AutomationAccountAzureEnvironment "AzureCloud"
 
 
@@ -7,11 +7,11 @@
 # & "C:\Code\CB\Azure\Antea\1-Migrate-From-UpdateManager.ps1" -AutomationAccountResourceId "/subscriptions/{subId}/resourceGroups/{rgName}/providers/Microsoft.Automation/automationAccounts/{aaName}" -AutomationAccountAzureEnvironment "AzureCloud"
 
 
-# & "$PSScriptroot\Migrate-From-UpdateManager.ps1" -AutomationAccountResourceId "/subscriptions/28c8da4f-f7e4-40b4-9fe8-fd53e4581d26/resourceGroups/defaultresourcegroup-cus/providers/Microsoft.Automation/automationAccounts/Automate-28c8da4f-f7e4-40b4-9fe8-fd53e4581d26-CUS" -AutomationAccountAzureEnvironment "AzureCloud"
+# & "$PSScriptroot\Migrate-From-UpdateManager.ps1" -AutomationAccountResourceId "/subscriptions/[AzSubID]/resourceGroups/defaultresourcegroup-cus/providers/Microsoft.Automation/automationAccounts/Automate-[AzSubID]-CUS" -AutomationAccountAzureEnvironment "AzureCloud"
 
 
 $scriptParams = @{
-    AutomationAccountResourceId = "/subscriptions/28c8da4f-f7e4-40b4-9fe8-fd53e4581d26/resourceGroups/defaultresourcegroup-cus/providers/Microsoft.Automation/automationAccounts/Automate-28c8da4f-f7e4-40b4-9fe8-fd53e4581d26-CUS"
+    AutomationAccountResourceId = "/subscriptions/[AzSubID]/resourceGroups/defaultresourcegroup-cus/providers/Microsoft.Automation/automationAccounts/Automate-[AzSubID]-CUS"
     AutomationAccountAzureEnvironment = "AzureCloud"
 }
 
