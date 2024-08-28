5827d3b0f10633a743e2defbd0bcfd7682bdd0d5
aollivierre <32661802+aollivierre@users.noreply.github.com>
2024-08-28 07:16:43 -0400

    Update 1-call.ps1

--- MigratefromAutomationAccount/1-call.ps1
+++ MigratefromAutomationAccount/1-call.ps1
@@ -1,8 +1,8 @@
-# & "C:\Code\CB\Azure\Antea\1-Migrate-From-UpdateManager.ps1" -AutomationAccountResourceId "/subscriptions/[AzSubID]/resourceGroups/defaultresourcegroup-cus/providers/Microsoft.Automation/automationAccounts/Automate-[AzSubID]-CUS
+# & "C:\Code\CB\Azure\[customer]\1-Migrate-From-UpdateManager.ps1" -AutomationAccountResourceId "/subscriptions/[AzSubID]/resourceGroups/defaultresourcegroup-cus/providers/Microsoft.Automation/automationAccounts/Automate-[AzSubID]-CUS
 # AzureCloud" -$AutomationAccountAzureEnvironment "AzureCloud"
 
 
-# & "C:\Code\CB\Azure\Antea\1-Migrate-From-UpdateManager.ps1" -AutomationAccountResourceId "/subscriptions/{subId}/resourceGroups/{rgName}/providers/Microsoft.Automation/automationAccounts/{aaName}" -AutomationAccountAzureEnvironment "AzureCloud"
+# & "C:\Code\CB\Azure\[customer]\1-Migrate-From-UpdateManager.ps1" -AutomationAccountResourceId "/subscriptions/{subId}/resourceGroups/{rgName}/providers/Microsoft.Automation/automationAccounts/{aaName}" -AutomationAccountAzureEnvironment "AzureCloud"
 
 
 # & "$PSScriptroot\Migrate-From-UpdateManager.ps1" -AutomationAccountResourceId "/subscriptions/[AzSubID]/resourceGroups/defaultresourcegroup-cus/providers/Microsoft.Automation/automationAccounts/Automate-[AzSubID]-CUS" -AutomationAccountAzureEnvironment "AzureCloud"
